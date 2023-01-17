71998dd8e565cc123d8b55c3f7d30a8dc2617c16
ASHISH BISHT <ashish23072002@gmail.com>
2023-01-17 22:00:10 +0530

    Sec Commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ function App() {
    <div>
     <Nav/>
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/profile" element={ <Profile/>}/>
-      <Route path="/product" element={ <Product/>}/>
+      <Route  exact path="/ShoppingInternProject" element={<Home/>}/>
+      <Route exact path="/about" element={<About/>}/>
+      <Route exact path="/profile" element={ <Profile/>}/>
+      <Route exact path="/product" element={ <Product/>}/>
     </Routes>
     <Footer/>
    </div>
