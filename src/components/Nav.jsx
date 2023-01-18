441d7f2132111a84f241d8b8dbcf19a4b0fe7eb9
ASHISH BISHT <ashish23072002@gmail.com>
2023-01-18 11:23:24 +0530

    Sec Commits

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -30,7 +30,7 @@ const Nav = () => {
           {
             Links.map((link) => (
               <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7 '>
-                <Link to={`${link.link}`} className='text-white  hover:text-gray-400 duration-500' >{link.name}</Link>
+                <Link to={{link}} className='text-white  hover:text-gray-400 duration-500' >{link.name}</Link>
                 {/* <a href={link.link} className='text-white  hover:text-gray-400 duration-500' >{link.name}</a> */}
               </li>
             ))
