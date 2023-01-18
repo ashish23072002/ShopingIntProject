7c42e62177ea995cbfffdcd854598f00940a0a3a
ASHISH BISHT <ashish23072002@gmail.com>
2023-01-18 16:23:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes,Route  } from 'react-router-dom';
+import { Routes,Route  } from 'react-router-dom';
 import About from './About';
 import './App.css';
 import Nav from './components/Nav';
